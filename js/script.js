e4ef71f63dc511c60fd0ba73a9c3bd436c44e13c
Roman Pchelkin <epos.91@mail.ru>
2022-03-08 16:43:49 +0300

    Complete HTML CSS JS
    Simple sound board

---
+++ js/script.js
@@ -0,0 +1,25 @@
+const sounds = ['dog1', 'dog2', 'dog3', 'dog4'];
+
+sounds.forEach(sound => {
+	const btn = document.createElement('button');
+	btn.classList.add('btn');
+
+	btn.innerText = sound;
+
+	btn.addEventListener('click', () => {
+		stopSongs();
+		document.getElementById(sound).play();
+	});
+
+	document.getElementById('buttons').
+	appendChild(btn);
+});
+
+function stopSongs() {
+	sounds.forEach(sound => {
+		const song = document.getElementById(sound);
+
+		song.pause();
+		song.currentTime = 0;
+	});
+}
